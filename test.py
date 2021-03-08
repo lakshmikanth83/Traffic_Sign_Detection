7cdc8b8a843f027918c0276d3f7c7848e6960e85
lakshmikanth83 <55424346+lakshmikanth83@users.noreply.github.com>
2021-03-08 12:31:53 -0500

    updated code

--- test.py
+++ test.py
@@ -1,5 +1,9 @@
 import pandas as pd
 import streamlit as st
 df = pd.read_csv('label_df.csv')
+
 st.title("Testing")
-st.write(df.head())
+num = 5
+num = st.text_input("Input the number of rows")
+num = int(num)
+st.write(df.head(num))
